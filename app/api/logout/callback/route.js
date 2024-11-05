57683c29b4feff83b4d6a965ef5098857cd55606
ricksterhd123 <ricksterhd123@gmail.com>
2024-11-05 08:43:38 +0000

    add logout functionality

--- app/api/logout/callback/route.js
+++ app/api/logout/callback/route.js
@@ -1,5 +1,7 @@
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+
 export async function GET(request) {
-  return Response.json({
-    message: 'Hello /logout/callback!'
-  });
+  const session = await getIronSession(await cookies(), { password: process.env.COOKIE_PASSWORD, cookieName: 'sessionId' });
+  session.destroy();
 }
--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -1,5 +1,13 @@
+import { idp, sp } from "@/lib/saml";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+
 export async function GET(request) {
-  return Response.json({
-    message: 'Hello world!'
-  });
+  const session = await getIronSession(await cookies(), { password: process.env.COOKIE_PASSWORD, cookieName: 'sessionId' });
+
+  const { id, context } = sp.createLogoutRequest(idp, 'redirect', session?.user?.login);
+  console.log(`logout request id:`, id);
+  console.log(`logout request context:`, context);
+
+  return Response.redirect(context);
 }
