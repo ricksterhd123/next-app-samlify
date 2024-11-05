ec548d04696e2727cf11bf496737962aa8181755
Rick <ricksterhd123@gmail.com>
2024-11-05 08:52:52 +0000

    set /api/logout/callback to POST

--- app/api/logout/callback/route.js
+++ app/api/logout/callback/route.js
@@ -1,7 +1,7 @@
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 
-export async function GET(request) {
+export async function POST(request) {
   const session = await getIronSession(await cookies(), { password: process.env.COOKIE_PASSWORD, cookieName: 'sessionId' });
   session.destroy();
 }
